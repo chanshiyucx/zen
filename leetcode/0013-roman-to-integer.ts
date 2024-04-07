332cabe02904fa963aea70bc6f4fcea2c650d252
chanshiyu <me@chanshiyu.com>
2024-04-07 15:54:14 +0800

    update

--- leetcode/0013-roman-to-integer.ts
+++ leetcode/0013-roman-to-integer.ts
@@ -1,23 +1,25 @@
 function romanToInt(s: string): number {
-  const map = [
-    [1000, "M"],
-    [900, "CM"],
-    [500, "D"],
-    [400, "CD"],
-    [100, "C"],
-    [90, "XC"],
-    [50, "L"],
-    [40, "XL"],
-    [10, "X"],
-    [9, "IX"],
-    [5, "V"],
-    [4, "IV"],
-    [1, "I"],
-  ] as [number, string][]
-
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
   let ans = 0
-  for (const [n, c] of map) {
+  for (let i = 0; i < s.length; i++) {
+    const cur = map[s[i]]
+    const next = map[s[i + 1]] ?? 0
+    if (cur < next) {
+      ans -= cur
+    } else {
+      ans += cur
+    }
   }
 
   return ans
 }
+
+console.log(romanToInt("LVIII"))
