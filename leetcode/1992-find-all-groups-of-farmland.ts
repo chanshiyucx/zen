3185febf27ee0cac710b9fd6df6322f46a3c3cc5
chanshiyu <me@chanshiyu.com>
2024-04-20 22:50:00 +0800

    update

--- leetcode/1992-find-all-groups-of-farmland.ts
+++ leetcode/1992-find-all-groups-of-farmland.ts
@@ -1,28 +1,26 @@
 function findFarmland(land: number[][]): number[][] {
   const ans = []
-  let temp = []
 
-  const dfs = (r1, c1, r2, c2) => {
-    if (r2 < 0 || r2 >= land.length || c2 < 0 || c2 >= land[0].length) return
+  const dfs = (r1, c1) => {
+    let r2 = r1
+    let c2 = c1
 
-    if (land[r2][c2] === 1) {
-      land[r2][c2] = 0
-      if (r2 > temp[2] || c2 > temp[3]) {
-        temp[2] = r2
-        temp[3] = c2
+    while (r2 < land.length && land[r2][c1] === 1) r2++
+    while (c2 < land[0].length && land[r1][c2] === 1) c2++
+
+    for (let i = r1; i <= r2 - 1; i++) {
+      for (let j = c1; j <= c2 - 1; j++) {
+        land[i][j] = 0
       }
-
-      dfs(r1, c1, r2 + 1, c2)
-      dfs(r1, c1, r2, c2 + 1)
     }
+
+    return [r1, c1, r2 - 1, c2 - 1]
   }
 
   for (let i = 0; i < land.length; i++) {
     for (let j = 0; j < land[0].length; j++) {
       if (land[i][j] === 1) {
-        temp = [i, j, i, j]
-        dfs(i, j, i, j)
-        ans.push(temp)
+        ans.push(dfs(i, j))
       }
     }
   }
