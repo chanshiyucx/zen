d093962384207ab00d1e05cd9dc48fc4ddd805fa
chanshiyu <me@chanshiyu.com>
2024-04-30 15:16:07 +0800

    update

--- leetcode/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/code.ts
+++ leetcode/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/code.ts
@@ -1,26 +1,6 @@
 function minOperations(nums: number[], k: number): number {
-  let finalXor = 0
-  // XOR of all integers in the array.
-  for (let n of nums) {
-    finalXor = finalXor ^ n
-  }
-
-  let count = 0
-  // Keep iterating until both k and finalXor becomes zero.
-  while (k > 0 || finalXor > 0) {
-    // k % 2 returns the rightmost bit in k,
-    // finalXor % 2 returns the rightmost bit in finalXor.
-    // Increment counter, if the bits don't match.
-    if (k % 2 != finalXor % 2) {
-      count++
-    }
-
-    // Remove the last bit from both integers.
-    k /= 2
-    finalXor /= 2
-  }
-
-  return count
+  // const
+  // return count
 }
 
 console.log(minOperations([2, 1, 3, 4], 1))
