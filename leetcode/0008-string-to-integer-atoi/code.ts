10f1fcd0fa6b5e2b4cf0d6783879220e8b47e3f2
chanshiyu <me@chanshiyu.com>
2024-07-10 17:52:59 +0800

    update

--- leetcode/0008-string-to-integer-atoi/code.ts
+++ leetcode/0008-string-to-integer-atoi/code.ts
@@ -1,14 +1,13 @@
 function myAtoi(s: string): number {
   let ans = 0
-  const match = s.match(/^\s*([\-|\+]?)(\d+)/)
+  const match = s.match(/^\s*([-+]?)(\d+)/)
   if (match) {
     ans = Number(match[2])
     if (match[1] === "-") {
       ans = ans * -1
     }
-    ans = Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, ans))
+    ans = Math.max(Math.pow(-2, 31), Math.min(Math.pow(2, 31) - 1, ans))
   }
-
   return ans
 }
 
