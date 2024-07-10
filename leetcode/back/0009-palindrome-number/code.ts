1dc6a6008251f1425a0b0009cd0ae4787cc2dab7
chanshiyu <chanshiyucx@gmail.com>
2024-07-10 22:51:57 +0800

    update

--- leetcode/back/0009-palindrome-number/code.ts
+++
@@ -1,9 +0,0 @@
-function isPalindrome(x: number): boolean {
-  if (x < 0) {
-    return false
-  }
-
-  return String(x) === Array.from(String(x)).reverse().join("")
-}
-
-console.log(isPalindrome(121))
